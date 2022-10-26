ffa4fdbc60734daf3d24201c5bc701667db28831
khanhdat612 <khanhdat612@gmail.com>
2022-10-26 17:18:59 +0700

    update

--- js/product.js
+++ js/product.js
@@ -1,9 +1,30 @@
-$('.product_item').slick({
-    dots: true,
-    infinite: true,
-    // arrows: false,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear',
+// $('.product_item').slick({
+//     dots: true,
+//     infinite: true,
+//     // arrows: false,
+//     speed: 500,
+//     fade: true,
+//     cssEase: 'linear',
 
+//   });
+
+  $('.product_combo_container').slick({
+    dots: true,
+    speed: 300,
+    slidesToShow: 1, 
+    infinite:false,       
+    variableWidth: true,
+    // centerMode: true,
+    // responsive: [
+    //   {
+    //       breakpoint: 650,
+    //       settings: {
+    //         dots: true,
+    //         speed: 300,
+    //         slidesToShow: 1,
+    //         infinite:true,
+    //     }
+    //   }
+    //   ],
+    
   });
